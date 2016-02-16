792ecbedc5189c75e6ca8b2b3ed48cab3f9412ee
liujie3612 <liujie3612@wilddog.com>
2016-02-16 17:00:48 +0800

    权限

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,6 +1,5 @@
 var ref = new Wilddog("https://task-management.wilddogio.com/");
-
 var user = $("#user").val();
-$("button").click(function () {
+$("button").click(function() {
     console.log(user);
 })

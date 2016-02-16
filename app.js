fe5a4de637c0ca71931dd0a60f9dfd17de75fe27
liujie3612 <liujie@wilddog.com>
2016-02-16 16:52:52 +0800

    1

---
+++ app.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var favicon = require('serve-favicon');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var hbs = require('hbs');
+var Wilddog = require("wilddog");
+var schedule = require("node-schedule");
+var nodemailer = require('nodemailer');
+
+var routes = require('./routes/index');
+var ref = new Wilddog("https://task-management.wilddogio.com/");
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/included');
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+app.set('view engine', 'hbs');
+
+app.use('/', routes);
+
+//邮件配置
+var config_email = {
+    host: 'smtp.mxhichina.com',
+    port: '25',
+    auth: {
+        user: 'liujie@wilddog.com',
+        pass: 'Liujie19900725'
+    }
+};
+
+var transporter = nodemailer.createTransport(config_email);
+
+var mailOptions = {
+    from: 'liujie@wilddog.com',
+    to: '1175667160@qq.com',
+    subject: 'Hello world',
+    text: 'Hello world'
+};
+
+/*transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Message sent: ' + info.response);
+    }
+});　*/
+
+var rule = new schedule.RecurrenceRule();
+rule.second = 0;
+//var date = new Date(2016, 1, 16, 15, 48, 0);
+var j = schedule.scheduleJob(rule, function () {
+    console.log('The world is going to end today.');
+});
+/*
+var j = schedule.scheduleJob(date, function () {
+    console.log('The world is going to end today.');
+});
+*/
+
+//报错
+module.exports = app;
---
+++ public/javascript/index.js
@@ -0,0 +1,6 @@
+var ref = new Wilddog("https://task-management.wilddogio.com/");
+
+var user = $("#user").val();
+$("button").click(function () {
+    console.log(user);
+})

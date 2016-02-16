7d3bb805faec09cef95c16431da51e610faeb2ec
liujie3612 <liujie3612@wilddog.com>
2016-02-16 23:34:45 +0800

    1

--- app.js
+++ app.js
@@ -18,43 +18,41 @@ hbs.registerPartials(__dirname + '/views/included');
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.set('view engine', 'hbs');
-
 app.use('/', routes);
 
 //取回数据
-/*ref.on('value', function(snap) {
-    var userText = snap.val().User;
-    var passText = snap.val().Pass;
-});*/
+ref.on('value', function(snap) {
+    var userText = snap.val().Auth.User;
+    var passText = snap.val().Auth.Pass;
+
+    //邮件配置
+    var config_email = {
+        host: 'smtp.mxhichina.com',
+        port: '25',
+        auth: {
+            user: userText,
+            pass: passText
+        }
+    };
+    var transporter = nodemailer.createTransport(config_email);
+    var mailOptions = {
+        from: userText,
+        to: '1175667160@qq.com',
+        subject: 'Hello world',
+        text: 'Hello world'
+    };
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Message sent: ' + info.response);
+        }
+    });　
+});
 
 
-//邮件配置
-var config_email = {
-    host: 'smtp.mxhichina.com',
-    port: '25',
-    auth: {
-        user: 'liujie@wilddog.com',
-        pass: 'Liujie19900725'
-    }
-};
-var transporter = nodemailer.createTransport(config_email);
 
-
-var mailOptions = {
-    from: 'liujie@wilddog.com',
-    to: '1175667160@qq.com',
-    subject: 'Hello world',
-    text: 'Hello world'
-};
-
-transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Message sent: ' + info.response);
-    }
-});　
-
+//定时设置
 var rule = new schedule.RecurrenceRule();
 rule.second = 0;
 //var date = new Date(2016, 1, 16, 15, 48, 0);
@@ -67,5 +65,4 @@ var j = schedule.scheduleJob(date, function () {
 });
 */
 
-//报错
 module.exports = app;
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,6 +1,29 @@
 var refAuth = new Wilddog("https://task-management.wilddogio.com/Auth");
 var refEmail = new Wilddog("https://task-management.wilddogio.com/Email");
 
+//初始化
+function init() {
+    refAuth.on('value', function(snap) {
+        $("#user").attr("value", snap.val().User);
+        $("#pass").attr("value", snap.val().Pass);
+    });
+
+    refEmail.on('value', function(snapshot) {
+        var snapshotLength = snapshot.numChildren();
+        console.log(snapshotLength);
+        for (var i = 0; i < snapshotLength; i++) {
+            var appendNode = $("<ul class = 'detials'><div class='timestamp'></div> <li> Subject:<input type = 'text ' class= 'subject' ></li> <li> To: <input type = 'text ' class = 'to ' > </li> <li> Content: <textarea class='content ' > </textarea> </li > <li> Time: <input type = 'text' class='time' > </li> <button class='send'>Send</button> </ul>");
+            $(".emails").append(appendNode);
+        };
+
+        snapshot.forEach(function(snap) {
+            $(".timestamp").text(snap.key());
+            console.log(snap.val());
+        });
+
+    });
+};
+init();
 $("#confirm").click(function() {
     var user = $("#user").val();
     var pass = $("#pass").val();
@@ -10,20 +33,26 @@ $("#confirm").click(function() {
     })
 });
 
-$("#send").click(function() {
-    var creatObj = {};
+
+$("#add").click(function() {
+    var appendNode = $("<ul class = 'detials'><div class='timestamp'></div> <li> Subject:<input type = 'text ' class= 'subject' ></li> <li> To: <input type = 'text ' class = 'to ' > </li> <li> Content: <textarea class='content ' > </textarea> </li > <li> Time: <input type = 'text' class='time' > </li> <button class='send'>Send</button> </ul>");
     var timeStamp = showTime();
-    var subject = $("#subject").val();
-    var to = $("#to").val();
-    var content = $("#content").val();
-    var time = $("#time").val();
-    creatObj[timeStamp] = {
-        "Subject": subject,
-        "To": to,
-        "Content": content,
-        "Time": time
-    };
-    refEmail.update(creatObj);
+    $(this).parent("ul").append(appendNode);
+    $(this).siblings('ul:last-child').children('.timestamp').text(timeStamp);
+    $(".send").click(function() {
+        var creatObj = {};
+        var subject = $(this).siblings('li').children('.subject').val();
+        var to = $(this).siblings('li').children('.to').val();
+        var content = $(this).siblings('li').children('.content').val();
+        var time = $(this).siblings('li').children('.time').val();
+        creatObj[timeStamp] = {
+            "Subject": subject,
+            "To": to,
+            "Content": content,
+            "Time": time
+        };
+        refEmail.update(creatObj);
+    });
 });
 
 function showTime() {
@@ -33,5 +62,6 @@ function showTime() {
     str += mydate.getDate() + "日";
     str += mydate.getHours() + "时";
     str += mydate.getMinutes() + "分";
+    str += mydate.getSeconds() + "秒";
     return str;
-}
+};

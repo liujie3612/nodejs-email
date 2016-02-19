1a12b5fc32fda5b38f3204ca2b3d62b365271429
liujie3612 <liujie3612@wilddog.com>
2016-02-19 14:44:19 +0800

    1

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ ref.on('value', function(snap) {
     var userText = snap.val().Auth.User;
     var passText = snap.val().Auth.Pass;
     var emailObj = snap.val().Email;
+    console.log(emailObj);
     var config_email = {
         host: 'smtp.mxhichina.com',
         port: '25',
@@ -33,30 +34,27 @@ ref.on('value', function(snap) {
             pass: passText
         }
     };
+    var transporter = nodemailer.createTransport(config_email);
 
     if (emailObj != undefined) {
         for (date in emailObj) {
             var EmailTo = emailObj[date].To;
             var EmailSub = emailObj[date].Subject;
             var EmailText = emailObj[date].Content;
-            // var EmailTime = emailObj[date].Time;
             var rule = emailObj[date].Time;
-            var transporter = nodemailer.createTransport(config_email);
             var mailOptions = {
                 from: userText,
                 to: EmailTo,
                 subject: EmailSub,
                 text: EmailText
             };
-
             //周期
-           
             var j = schedule.scheduleJob(rule, function() {
                 transporter.sendMail(mailOptions, function(error, info) {
                     if (error) {
                         console.log(error);
                     } else {
-                        console.log('Message sent: ' + info.response + info.messageId + info.envelope + info.accepted +  info.rejected + info.pending);
+                        console.log('Message sent: ' + info.response + info.accepted);
                     }
                 })
             })
